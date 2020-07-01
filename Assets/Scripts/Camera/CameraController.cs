00355aebacd27002c57670fc5a7508333ceb6aa2
Ian Brock <ibpositive@live.com>
2020-06-30 23:56:07 -0400

    6g
    Test fixes.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float _tilt;
+
+    private void Update()
+    {
+        float mouseRoataion = Input.GetAxis("Mouse Y");
+        // to restrict how much the player can move the camera
+        _tilt = Mathf.Clamp(_tilt - mouseRoataion, -15f, 15f);
+        // the reason we edit the x axis instead of y when using "Mouse Y" has something
+        // to do with 2d vs 3d and how they are viewed therein.
+        transform.localRotation = Quaternion.Euler(_tilt, 0f, 0f);
+    }
+}
