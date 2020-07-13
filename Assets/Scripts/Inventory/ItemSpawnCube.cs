c44a1208a7f893a879caaf95052a06787dbbc47c
Ian Brock <ibpositive@live.com>
2020-07-13 14:30:42 -0400

    Debugging complete.
    The fix was commenting out code for an unused line in ItemSpawnCube.cs. Comments in the file go into detail about it.

--- Assets/Scripts/Inventory/ItemSpawnCube.cs
+++ Assets/Scripts/Inventory/ItemSpawnCube.cs
@@ -1,12 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemSpawnCube : ItemComponent
 {
-    private Player player = GameObject.FindObjectOfType<Player>();
+    // Unity is giving me errors about FindObjectsOfType.
+    // It asked to throw it in an awake or start method, but that caused a bunch of other errors.
+    // Commenting out the line seems to fix it.
+    // No isses as a result of this have occured yet. Also, it shouldn't because this player was never used.
+    //private Player player = GameObject.FindObjectOfType<Player>();
     public GameObject itemPrefab;
-    
+
+    private void Awake()
+    {
+        //throw new NotImplementedException();
+        //private Player player = GameObject.FindObjectOfType<Player>();
+    }
+
     public override void Use()
     {
         Instantiate(itemPrefab, new Vector3(0, 0, 10), Quaternion.identity);
