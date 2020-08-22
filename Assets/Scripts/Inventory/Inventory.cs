ac3dbf6a61c1a171d7547339f4b6708197b6168c
Ian Brock <ibpositive@live.com>
2020-08-22 14:59:45 -0400

    18a
    Inventory Slot tests

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -7,18 +8,20 @@ using UnityEngine;
 /// </summary>
 public class Inventory : MonoBehaviour
 {
+    private const int DEFAULT_INVENTORY_SIZE = 25;
     public event Action<Item> ActiveItemChanged;
     public event Action<Item> ItemPickedUp;
 
     [SerializeField] private Transform _rightHand;
 
-    private List<Item> _items = new List<Item>();
+    private Item[] _items = new Item[DEFAULT_INVENTORY_SIZE];
     private Transform _itemRoot;
+    
 
     public Item ActiveItem { get; private set; }
-    public List<Item> Items => _items;
- 
-        
+    public List<Item> Items => _items.ToList();
+    public int Count => _items.Count(t => t != null);
+
     // We make the awake to initialize _item and _itemRoot.
     private void Awake()
     {
@@ -26,14 +29,36 @@ public class Inventory : MonoBehaviour
         _itemRoot.transform.SetParent(transform);
     }
 
-    public void Pickup(Item item)
+    public void Pickup(Item item, int? slot = null)
     {
-        _items.Add(item);
+        if (slot.HasValue == false)
+        {
+            slot = FindFirstAvailableSlot();
+        }
+
+        if (slot.HasValue == false)
+        {
+            return;
+        }
+        _items[slot.Value] = item;
         item.transform.SetParent(_itemRoot);
         ItemPickedUp?.Invoke(item);
         item.WasPickedUp = true;
-        
+
         Equip(item);
+    }
+
+    private int? FindFirstAvailableSlot()
+    {
+        for (int i = 0; i < _items.Length; i++)
+        {
+            if (_items[i] == null)
+            {
+                return i;
+            }
+        }
+
+        return null;
     }
 
     public void Equip(Item item)
@@ -43,7 +68,7 @@ public class Inventory : MonoBehaviour
             ActiveItem.transform.SetParent(_itemRoot);
             ActiveItem.gameObject.SetActive(false);
         }
-        
+
         Debug.Log($"Equipped Item {item.gameObject.name}");
         item.transform.SetParent(_rightHand);
         item.transform.localPosition = Vector3.zero;
@@ -52,5 +77,10 @@ public class Inventory : MonoBehaviour
         // if active item is not null then invoke
         // ? is making a null propagation method
         ActiveItemChanged?.Invoke(ActiveItem);
+    }
+
+    public Item GetItemInSlot(int slot)
+    {
+        return _items[slot];
     }
 }
